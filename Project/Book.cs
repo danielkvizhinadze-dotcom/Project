51796c24c46dad1de22ec07955b0990a98c4ab3a
danie <danie@DYEERIK>
2025-12-02 18:24:34 +0400

    Add project files.

---
+++ Project/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project
+{
+    internal class Book
+    {
+        public Book() //კონსტრუქტორი
+        { 
+
+        }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int ReleaseYear { get; set; }
+    }
+}
---
+++ Project/BookManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace Project
+{
+    internal class BookManager
+    {
+        private List<Book> books = new List<Book>();
+
+    }
+}
