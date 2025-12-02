866b44f45f9d09703a5e05cc4cf0eb3cab7612db
danielkvizhinadze-dotcom <danielkvizhinadze@gmail.com>
2025-12-02 19:06:14 +0400

    Input

--- Project/Program.cs
+++ Project/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System.Diagnostics;
+
+Console.WriteLine("Enter 1 to add a book");
+Console.WriteLine("Enter 2 to see all current books");
+Console.WriteLine("Enter 3 to search for a book");
+
+var UserInput=Console.ReadKey(true).Key;
+switch (UserInput)
+{
+    case ConsoleKey.D1:
+    case ConsoleKey.F1:
+        break;
+    default:
+        Console.WriteLine("wrong input"); break;
+}
