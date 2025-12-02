58e812c6b7d09d916c9ce78f722689e78d6b3c62
danielkvizhinadze-dotcom <danielkvizhinadze@gmail.com>
2025-12-02 18:38:57 +0400

    Remove book

--- Project/BookManager.cs
+++ Project/BookManager.cs
@@ -6,6 +6,17 @@ namespace Project
     internal class BookManager
     {
         private List<Book> books = new List<Book>();
-
+        public void AddBook(Book book)
+        {
+            books.Add(book);
+        }
+        public void RemoveBook(Book book)
+        {
+            var Result = books.FirstOrDefault(b => b.Title == book.Title);
+            if (Result != null)
+            {
+                books.Remove(Result);
+            }
+        }
     }
 }
