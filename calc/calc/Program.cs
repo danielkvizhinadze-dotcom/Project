e6c002161e7e2528819adf57588089318edf2b6c
danielkvizhinadze-dotcom <danielkvizhinadze@gmail.com>
2025-12-02 19:18:58 +0400

    Add files via upload

---
+++ calc/calc/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("Enter a Number");
+double Num1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Enter another number");
+double Num2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Add, subtract, divide or multiply?");
+
+char Symbol = Convert.ToChar(Console.ReadLine());
+
+double Result = 0;
+
+if (Symbol == '+')
+{
+    Result = Num1 + Num2;
+}
+if (Symbol == '-')
+{
+    Result = Num1 - Num2;
+}
+if (Symbol == '/')
+{
+    Result = Num1 / Num2; 
+}
+if (Symbol == '*')
+{
+    Result = Num1 * Num2; 
+}
+
+Console.WriteLine("The answer is:");
+Console.WriteLine(Result);
